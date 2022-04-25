75e3bffc4d124cbc3318d0775e2ffcb00cc9efa6
VadymVadymov <vadymvadymov@users.noreply.github.com>
2022-04-25 15:27:31 +0200

    `fizzBuzz` semantic rework

--- src/Challenges/2.0-FizzBuzz.purs
+++ src/Challenges/2.0-FizzBuzz.purs
@@ -15,16 +15,15 @@ Write a function which prints ints from 1 to 100 where all of ones divided by
 fizzBuzz :: Effect Unit
 fizzBuzz = go 1
   where
-  go i =
+  go i = do
     let
       isDivBy3 = if mod i 3 == 0 then true else false
       isDivBy5 = if mod i 5 == 0 then true else false
-      print v = Console.log v >>= \_ -> go (i + 1)
-    in
-      if i == 100 then Console.logShow i
-      else case isDivBy3, isDivBy5 of
-        true, true -> print "FizzBuzz"
-        true, _ -> print "Fizz"
-        _, true -> print "Buzz"
-        _, _ -> print $ show i
-
+      print v =
+        if i < 100 then Console.log v >>= \_ -> go (i + 1)
+        else Console.logShow i
+    case isDivBy3, isDivBy5 of
+      true, true -> print "FizzBuzz"
+      true, _ -> print "Fizz"
+      _, true -> print "Buzz"
+      _, _ -> print $ show i
