8ac0955270881ebead675577cb14eaead8f0c11d
VadymVadymov <vadymvadymov@users.noreply.github.com>
2022-04-25 20:30:53 +0200

    `fizzBuzz` implement with flexible config

--- src/Challenges/2.0-FizzBuzz.purs
+++ src/Challenges/2.0-FizzBuzz.purs
@@ -2,6 +2,8 @@ module FizzBuzz where
 
 import Prelude
 
+import Data.Array as Array
+import Data.String as String
 import Effect (Effect)
 import Effect.Console as Console
 
@@ -12,7 +14,7 @@ Write a function which prints ints from 1 to 100 where all of ones divided by
 
 -}
 
-fizzBuzz :: Effect Unit
+fizzBuzz :: Effect Unit -- Standart version
 fizzBuzz = go 1
   where
   go i = do
@@ -20,10 +22,29 @@ fizzBuzz = go 1
       isDivBy3 = if mod i 3 == 0 then true else false
       isDivBy5 = if mod i 5 == 0 then true else false
       print v =
-        if i < 100 then Console.log v >>= \_ -> go (i + 1)
-        else Console.logShow i
+        if i <= 100 then Console.log v >>= \_ -> go (i + 1)
+        else pure unit
     case isDivBy3, isDivBy5 of
       true, true -> print "FizzBuzz"
       true, _ -> print "Fizz"
       _, true -> print "Buzz"
       _, _ -> print $ show i
+
+type Cfg = Array { num :: Int, word :: String }
+
+fizzBuzzCfg :: Effect Unit
+fizzBuzzCfg = go 1 cfg
+  where
+  go i arr =
+    let
+      output = Array.fold $ arr
+        <#> (\{ num, word } -> if mod i num == 0 then word else mempty)
+      print v =
+        if i <= 100 then Console.log v >>= \_ -> go (i + 1) arr else pure unit
+    in
+      if String.null output then print $ show i else print output
+  cfg =
+    [ { num: 3, word: "Fizz" }
+    , { num: 5, word: "Buzz" }
+    , { num: 10, word: "Bruh" }
+    ]
